472cf7dcf452c63478a949de99e54bdd7a9e70bb
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-24 15:13:48 +0900

    css1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
+import { CssModules } from "./components/CssModules";
 import { InlineStyle } from "./components/inlineStyle";
 import "./styles.css";
 
 export default function App() {
-  const containerStyle = {
-    border: "solid 2px #392eff",
-    borderRadius: "20px",
-    padding: "8px",
-    margin: "8px",
-    display: "flex",
-    justifyContents: "space-around",
-    alineItem: "center"
-  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
